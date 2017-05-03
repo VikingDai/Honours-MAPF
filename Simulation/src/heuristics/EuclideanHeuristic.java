6dae8c62dd9c10b4c9e831a383b744e12039904f
Phillip Wong <func.pwong@gmail.com>
2017-05-03 15:26:56 +1000

    Updated simulation - removed getters and setters

---
+++ Simulation/src/heuristics/EuclideanHeuristic.java
@@ -0,0 +1,38 @@
+package heuristics;
+
+import domains.GridMapMetaInfo;
+
+public class ManhattanHeuristic extends BaseHeuristic
+{
+    private int mapWidth;
+    private int mapHeight;
+
+    public ManhattanHeuristic(int mapWidth, int mapHeight)
+    {
+        this.mapWidth = mapWidth;
+        this.mapHeight = mapHeight;
+    }
+
+    public ManhattanHeuristic(GridMapMetaInfo mapMetaInfo)
+    {
+        this(mapMetaInfo.getWidth(), mapMetaInfo.getHeight());
+    }
+
+    @Override
+    public int h(int x1, int y1, int x2, int y2)
+    {
+        return (Math.abs(x2 - x1) + Math.abs(y2 - y1));
+    }
+
+    @Override
+    public int h(int startId, int goalId)
+    {
+        int xS = startId % mapWidth;
+        int yS = startId / mapWidth;
+
+        int xE = goalId % mapWidth;
+        int yE = goalId / mapWidth;
+
+        return h(xS, yS, xE, yE);
+    }
+}
